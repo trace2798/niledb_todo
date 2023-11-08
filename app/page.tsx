f5b47ac0958a4345b4dea1775d212bca10d9817b
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-11-08 17:08:07 +0530

    small change in word of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <main className="flex h-[77vh] flex-col items-center justify-center p-24">
       <h1 className="text-3xl max-w-xl text-center">
-        This is a Demo Todo Application made using Nile DB, Next.js and
-        shadcn/ui
+        Another Todo Application made using Nile DB, Next.js and shadcn/ui
       </h1>
     </main>
   );
